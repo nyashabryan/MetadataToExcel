d6b0b4d0f747d5649926a0285bbea0b2764e84f3
nyashabryan <nyashabryan23@gmail.com>
2018-09-05 00:49:37 +0200

    Add project files.

---
+++ ExtractorOnCommandLine/Image.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ExtractorOnCommandLine
+{
+    class Image
+    {
+        private readonly string ImageLocation;
+        private readonly string ImageName;
+        private readonly DateTime DateTaken;
+        private readonly Device CameraDevice;
+        private readonly GPS Location;
+        private readonly string UniqueID;
+        
+        public Image()
+        {
+            ;
+        }
+
+        private DateTime MakeDate(string dateTaken)
+        {
+            return new DateTime();
+        }
+
+        override
+        public string ToString()
+        {
+            return this.ImageLocation;
+        }
+
+        class GPS
+        {
+            private readonly decimal longitude;
+            private readonly decimal latitude;
+
+            private GPS(decimal longitude, decimal latitude)
+            {
+                this.longitude = longitude;
+                this.latitude = latitude;
+            }
+
+            public static GPS FromDegrees(decimal longitude, decimal latitude)
+            {
+                return new GPS(longitude, latitude);
+            }
+            
+            public static GPS FromDegrees(string location)
+            {
+                var r = new Regex(@"^[0-9]+.[0-9]+. \w [0-9]+.[0-9]+. \w$");
+                if (!r.IsMatch(location)) throw new SystemException(
+                    "Invalid format of GPS coordinates.");
+                string[] parts = location.Split(' ');
+                string longitudeSign = "";
+                string latitudeSign = "";
+                if (parts[0].Equals('W'))
+                {
+                    longitudeSign = "-";
+                }
+                if (parts[1].Equals('S'))
+                {
+                    latitudeSign = "-";
+                }
+                return new GPS(
+                    decimal.Parse($"{longitudeSign}{parts[0]}"),
+                    decimal.Parse($"{latitudeSign}{parts[2]}")
+                    );
+            }
+
+            public static GPS FromDegreesSecondsMinutes(string location)
+            {
+                var r = new Regex(
+                    @"^([0-9]+. [0-9]+. [0-9]+. \w( )*)+$");
+                var r2 = new Regex(
+                    @"^([0-9]+.[0-9]+.[0-9]+. \w( )*)+$");
+                if (r2.IsMatch(location))
+                {
+                    string[] parts = location.Split(' ');
+                    if (!parts.Length.Equals(8))
+                        throw new SystemException("Invalid format of GPS coordinates.");
+
+                    decimal longitude = GPS.DegreesMinutesSecondsToDegrees(
+                        decimal.Parse(parts[0]),
+                        decimal.Parse(parts[1]),
+                        decimal.Parse(parts[2])
+                        );
+
+                    decimal latitude = GPS.DegreesMinutesSecondsToDegrees(
+                        decimal.Parse(parts[4]),
+                        decimal.Parse(parts[5]),
+                        decimal.Parse(parts[6])
+                        );
+
+                    if (parts[3].Equals('W'))
+                        longitude *= -1;
+                    if (parts[7].Equals('S'))
+                        latitude *= -1;
+
+                    return new GPS(longitude, latitude);
+                }
+                else
+                {
+                    throw new SystemException("Invalid format of GPS coordinates.");
+                }
+            }
+
+            public static decimal DegreesMinutesSecondsToDegrees(
+                decimal degrees, decimal minutes, decimal seconds
+                )
+            {
+                return degrees + minutes / 60m + seconds / 3600m;
+            }
+        }
+
+        class Device
+        {
+            private readonly string device;
+            private readonly string model;
+            private readonly string software;
+
+            Device() : this("", "", "")
+            {
+
+            }
+
+            Device(string device, string model, string software)
+            {
+                this.device = device;
+                this.model = model;
+                this.software = software;
+            }
+
+            override
+            public string ToString()
+            {
+                return $"{this.device} : {this.model} : {this.software}";
+            }
+        }
+    }
+}
---
+++ ExtractorOnCommandLine/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MetadataExtractor;
+using MetadataExtractor.Formats.Exif;
+using MetadataExtractor.Formats.Exif.Makernotes;
+
+namespace ExtractorOnCommandLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.FileInfo file = new System.IO.FileInfo(@"C:\Users\Nyashadzaishe Bryan\VS Projects\ExtractorOnCommandLine\image3.jpg");
+            System.IO.FileInfo[] files = { file };
+            List<Image> imagesList = new List<Image>();
+            Image currentImage;
+
+            for (var i = 0; i < files.Length; i++)
+            {
+                Console.WriteLine();
+                if (!(files[i].Extension.Equals(".jpg") ||
+                    files[i].Extension.Equals(".JPG")))
+                {
+                    continue;
+                }
+                Console.WriteLine($"{files[i].FullName}\n");
+                currentImage = new Image();
+                IReadOnlyList<Directory> directories;
+                try
+                {
+                    directories = ImageMetadataReader.ReadMetadata(files[i].FullName);
+                } catch (ImageProcessingException e)
+                {
+                    Console.WriteLine($"\n {files[i].FullName} could not be read properly.");
+                    Console.WriteLine(e.ToString());
+                    continue;
+                } catch (System.IO.IOException e)
+                {
+                    Console.WriteLine($"\n {files[i].FullName} could not be read properly.");
+                    Console.WriteLine(e.ToString());
+                    continue;
+                }
+
+                foreach (Directory directory in directories)
+                {
+                    Console.WriteLine($"{directory.ToString()}\n");
+                    
+                    foreach(Tag tag in directory.Tags)
+                    {
+                        Console.WriteLine("{0, 20}: {1, 20}", tag.Name, tag.Description);
+                    }
+                }
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Press Any Key to Exit");
+            Console.ReadKey();
+        }
+
+        public static void PrintImagesResults(List<Image> listOfImages)
+        {
+            foreach(Image image in listOfImages)
+            {
+                
+            }
+        }
+    }
+}
